1456f03c60be4f8a50cf827000eaded37d1e8a5e
MTIT396 <minhthienit39@gmail.com>
2025-11-21 23:44:31 +0700

    fix: .tsx remove when import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import TempProvider from "./contexts/TempContext.tsx";
+import App from "./App";
+import TempProvider from "./contexts/TempContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
